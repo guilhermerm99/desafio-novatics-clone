f4f51f8dd0157d1fc0d9cf31b85f2dcd8b7310a8
Guilherme Ribeiro <guilhermerm99@gmail.com>
2022-02-07 19:22:24 -0300

    Adicionei algumas constantes da parte lógica.

--- problema2/index.js
+++ problema2/index.js
@@ -28,7 +28,12 @@ let validadorDeSudoku = function(board) {
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-
+            const celula = board[i][j]
+            if (celula === '.') continue
+            const numeroDaCaixa = 3 * Math.floor(i / 3) + Math.floor(j / 3)
+            const linha = `Linha: ${i}, valor ${celula}`
+            const coluna = `Coluna: ${j}, valor ${celula}`
+            const miniCaixa = `Mini Caixa: ${numeroDaCaixa}, valor ${celula}`
         }
     }
 
